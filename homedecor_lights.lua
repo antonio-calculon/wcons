cbba4a9ab1d463d75487beb807d4d8b9eb67f75d
cedric <cedric@loulou.home>
2017-04-16 15:54:16 +0200

    added some homedecor lights

--- homedecor_lights.lua
+++ homedecor_lights.lua
@@ -1,2 +1,15 @@
 
 wcons.register_light_device("homedecor:ground_lantern", { "homedecor:ground_lantern" })
+wcons.register_light_device("homedecor:ceiling_lantern", { "homedecor:ceiling_lantern" })
+wcons.register_light_device("homedecor:hanging_lantern", { "homedecor:hanging_lantern" })
+wcons.register_light_device("homedecor:wall_lamp", { "homedecor:wall_lamp" })
+wcons.register_light_device("homedecor:lattice_lantern_large", { "homedecor:lattice_lantern_large" })
+wcons.register_light_device("homedecor:lattice_lantern_small", { "homedecor:lattice_lantern_small" })
+wcons.register_light_device("homedecor:ceiling_lamp", { "homedecor:ceiling_lamp_off", "homedecor:ceiling_lamp" })
+wcons.register_light_device("homedecor:plasma_lamp", { "homedecor:plasma_lamp" })
+
+wcons.register_light_device("homedecor:glowlight_half", { "homedecor:glowlight_half" })
+wcons.register_light_device("homedecor:glowlight_quarter", { "homedecor:glowlight_quarter" })
+wcons.register_light_device("homedecor:glowlight_small_cube", { "homedecor:glowlight_small_cube" })
+
+wcons.register_light_device("lavalamp:lavalamp", { "lavalamp:lavalamp_off", "lavalamp:lavalamp" })
