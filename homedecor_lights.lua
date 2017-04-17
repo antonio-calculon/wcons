218528d39269d9f3c42212c23339ab2d4b59b9d4
antonio-calculon <cedric@loulou.home>
2017-04-17 20:07:00 +0200

    added the plasma ball and ceiling fan devices

--- homedecor_lights.lua
+++ homedecor_lights.lua
@@ -7,6 +7,7 @@ wcons.register_light_device("homedecor:lattice_lantern_large", { "homedecor:latt
 wcons.register_light_device("homedecor:lattice_lantern_small", { "homedecor:lattice_lantern_small" })
 wcons.register_light_device("homedecor:ceiling_lamp", { "homedecor:ceiling_lamp_off", "homedecor:ceiling_lamp" })
 wcons.register_light_device("homedecor:plasma_lamp", { "homedecor:plasma_lamp" })
+wcons.register_light_device("homedecor:plasma_ball", { "homedecor:plasma_ball" })
 
 wcons.register_light_device("homedecor:glowlight_half", { "homedecor:glowlight_half" })
 wcons.register_light_device("homedecor:glowlight_quarter", { "homedecor:glowlight_quarter" })
@@ -28,4 +29,13 @@ wcons.register_light_device("homedecor:table_lamp_off", {
     "homedecor:table_lamp_med",
     "homedecor:table_lamp_hi",
     "homedecor:table_lamp_max",
+})
+
+wcons.register_light_device("homedecor:ceiling_fan", { "homedecor:ceiling_fan" })
+minetest.override_item("wcons:ceiling_fan_0", {
+    tiles = {
+        "[combine:64x64:0,0=homedecor_ceiling_fan_top.png",
+        "[combine:64x64:0,0=homedecor_ceiling_fan_bottom.png",
+		"homedecor_ceiling_fan_sides.png",
+    },
 })
